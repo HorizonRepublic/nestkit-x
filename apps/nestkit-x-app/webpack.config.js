c1da36f490a30125a927508db3b8453f0ceef01c
Kyrylo Lytvynov <themaiby0@gmail.com>
2025-06-16 13:18:55 +0300

    chore(): add example project; linter setup

---
+++ apps/nestkit-x-app/webpack.config.js
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+const { join } = require('path');
+
+const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
+
+module.exports = {
+  output: {
+    path: join(__dirname, '../../dist/apps/nestkit-x-app'),
+  },
+  plugins: [
+    new NxAppWebpackPlugin({
+      target: 'node',
+      compiler: 'tsc',
+      main: './src/main.ts',
+      tsConfig: './tsconfig.app.json',
+      assets: ['./src/assets'],
+      optimization: false,
+      outputHashing: 'none',
+      generatePackageJson: true,
+    }),
+  ],
+};
