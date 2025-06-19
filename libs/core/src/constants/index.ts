d89a09990d99ca5e2f63f1d3cced6414ad15228e
Kyrylo Lytvynov <themaiby0@gmail.com>
2025-06-19 11:45:43 +0300

    chore(): force early loading priority for logger

--- libs/core/src/constants/index.ts
+++ libs/core/src/constants/index.ts
@@ -1,6 +1,4 @@
 import { network } from './network';
 import * as patterns from './patterns';
 
-export const ENV_METADATA_KEY = Symbol('env-metadata');
-
 export const constants = { network, patterns };
