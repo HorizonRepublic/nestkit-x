8f4443c90ed127cd1f9ad4e547207d6289be87d4
Kyrylo Lytvynov <themaiby0@gmail.com>
2025-07-17 14:16:12 +0300

    feat(jetstream-transport): remove PushStrategy and simplify transport strategy initialization

--- libs/jetstream-transport/src/jetstream.context.ts
+++ libs/jetstream-transport/src/jetstream.context.ts
@@ -1,25 +1,14 @@
 import { BaseRpcContext } from '@nestjs/microservices/ctx-host/base-rpc.context';
-import { JsMsg, Msg } from 'nats';
+import { JsMsg } from 'nats';
 
 export type NatsJetStreamContextArgs = [JsMsg];
-export type NatsContextArgs = [Msg];
 
 export class JetStreamContext extends BaseRpcContext<NatsJetStreamContextArgs> {
-  constructor(args: NatsJetStreamContextArgs) {
+  public constructor(args: NatsJetStreamContextArgs) {
     super(args);
   }
 
-  get message(): JsMsg {
-    return this.args[0];
-  }
-}
-
-export class NatsContext extends BaseRpcContext<NatsContextArgs> {
-  constructor(args: NatsContextArgs) {
-    super(args);
-  }
-
-  get message(): Msg {
+  public get message(): JsMsg {
     return this.args[0];
   }
 }
