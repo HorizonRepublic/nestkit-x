26b4dd68b92075749b40159a3392010fa82999e9
Kyrylo Lytvynov <themaiby0@gmail.com>
2025-07-17 14:16:12 +0300

    feat(jetstream-transport): remove PushStrategy and simplify transport strategy initialization

--- libs/jetstream-transport/src/jetstream.transport.ts
+++ libs/jetstream-transport/src/jetstream.transport.ts
@@ -1,31 +1,12 @@
 import { CustomStrategy } from '@nestjs/microservices';
-
-import { JetstreamPushStrategy } from './strategies/jetstream-push.strategy';
 import { JetstreamPullStrategy } from './strategies/jetstream-pull.strategy';
 import { IJetstreamTransportOptions } from './types/jetstream-transport.options';
-import { JetstreamTransportStrategy } from './conts';
-import { RuntimeException } from '@nestjs/core/errors/exceptions';
 
 export class JetstreamTransport implements CustomStrategy {
-  public strategy!: JetstreamPushStrategy | JetstreamPullStrategy;
+  public strategy!: JetstreamPullStrategy;
   public options: Record<string, string> = {};
 
   public constructor(options: IJetstreamTransportOptions) {
-    this.strategy = this.createStrategy(options);
-  }
-
-  private createStrategy(
-    options: IJetstreamTransportOptions,
-  ): JetstreamPushStrategy | JetstreamPullStrategy {
-    const strategies = {
-      [JetstreamTransportStrategy.Push]: () => new JetstreamPullStrategy(options),
-      [JetstreamTransportStrategy.Pull]: () => new JetstreamPushStrategy(options),
-    };
-
-    const strategyFactory = strategies[options.jetStreamStrategy];
-
-    if (!strategyFactory) throw new RuntimeException(`Unknown strategy: ${options.jetStreamStrategy}`);
-
-    return strategyFactory();
+    this.strategy = new JetstreamPullStrategy(options);
   }
 }
